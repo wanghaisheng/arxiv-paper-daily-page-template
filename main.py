b8757279ef5cce2935ba1894e55fcb8c3191dae1
HeisenBerg? <edwin_uestc@163.com>
2023-07-26 15:03:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -305,11 +305,6 @@ class _OverloadTasks:
         f"tags: ['brand','brand monitor']\r" \
         f"theme: 'light'\r" \
         f"featured: true\r" \
-        f"meta:\r" \
-        f" - name: author\r" \
-        f"   content: 作者是我\r" \
-        f" - name: keywords\r" \
-        f"   content: key3, key4\r" \
         f"keywords: key1, key2, key3\r" \
         f"---" \
         f"\r" \
