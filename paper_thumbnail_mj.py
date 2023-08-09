364245e1f45a0ab1b4405ba80877a50bad0cdd64
HeisenBerg? <edwin_uestc@163.com>
2023-08-09 09:41:27 +0800

    Update paper_thumbnail_mj.py

--- paper_thumbnail_mj.py
+++ paper_thumbnail_mj.py
@@ -12,6 +12,7 @@ def get_image_urls(url):
     body = soup.find("body")
     sc = list(body.find_all("script"))[-1].string
     sc = str(sc)
+    print(soup)
     script = soup.find('script', {'id': '__NEXT_DATA__'})
     sc = script.contents[0]    
     print(sc)
