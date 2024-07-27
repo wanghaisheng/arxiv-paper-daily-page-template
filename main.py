f7c9d7f79aee530705b74fe0ec5c53fd1434620d
HeisenBerg? <edwin_uestc@163.com>
2024-07-27 15:59:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ class CoroutineSpeedup:
             max_results=self.max_results,
             sort_by=arxiv.SortCriterion.SubmittedDate
         ).results()
-
+        print(f'query results:{len(res)}')
         context.update({"response": res, "hook": context})
         self.worker.put_nowait(context)
 
